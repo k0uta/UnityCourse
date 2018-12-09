3023e24dc81818b4d10b170537472433a4b9b3ab
Murilo Costa <murilo.fioravanti.costa@gmail.com>
2018-12-09 18:30:09 -0200

    Basic patrol behaviour
    Closes #3

---
+++ FinalProject/Assets/_Scripts/PatrolBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.AI;
+
+public class PatrolBehaviour : NetworkBehaviour {
+
+    public List<Transform> patrolPoints;
+
+    NavMeshAgent agent;
+
+    int nextIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        GoToNextPoint();
+
+    }
+
+    public override void OnStartServer()
+    {
+    }
+
+    private void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        if(patrolPoints.Count < 1)
+        {
+            return;
+        }
+
+        agent.SetDestination(patrolPoints[nextIndex].position);
+
+        if(++nextIndex >= patrolPoints.Count)
+        {
+            nextIndex = 0;
+        }
+    }
+}
